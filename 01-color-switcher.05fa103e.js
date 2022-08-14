01556af5eecfca1d61b5fbb2b407eea1185a6c6a
panivnyk <98265791+panivnyk@users.noreply.github.com>
2022-08-14 18:38:11 +0000

    Deploying to gh-pages from @ panivnyk/goit-js-hw-09@6b0df5920f877b6eb18b22bf1f5e14cc05f10694 🚀

---
+++ 01-color-switcher.05fa103e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;t.disabled=!0;e.addEventListener("click",(()=>{d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.05fa103e.js.map
